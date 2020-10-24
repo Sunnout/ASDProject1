1682c0bc666f975d7d32afbb89e5dd58cc5bca29
AndreOA <a.atalaia@campus.fct.unl.pt>
2020-10-24 19:04:16 +0100

    Added WhiteSpace

--- Script.sh
+++ Script.sh
@@ -7,5 +7,6 @@ do
 	myScript='java -jar target/asdProj.jar -conf config.properties contact=127.0.0.1:5000 port="'"$TH"'"; exec bash'
 	script='echo "arg 1 is:"'" $TH"'"";exec bash  '
 	gnome-terminal -- /bin/sh -c "$myScript"
+
 done
 
