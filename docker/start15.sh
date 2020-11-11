cefabf0cf3286b29615fe3997ab5702d157c7330
Ema Vieira <er.vieira@campus.fct.unl.pt>
2020-11-11 20:28:07 +0000

    Added hyparview clear time

--- docker/start15.sh
+++ docker/start15.sh
@@ -6,5 +6,5 @@ try=$3
 shift
 shift
 shift
-java -DlogFilename=logs/node$idx-t15-$try -cp asdProj.jar Main -conf config.properties dissemination_and_membership=3 payload_size=1024 broadcast_interval=200 "$@" &> /proc/1/fd/1
+java -DlogFilename=logs/node$idx-t15-$try -cp asdProj.jar Main -conf config.properties dissemination_and_membership=3 payload_size=1024 broadcast_interval=200 clear_received_time=7000 "$@" &> /proc/1/fd/1
 chown $user logs/node$idx-t15-$try.log
