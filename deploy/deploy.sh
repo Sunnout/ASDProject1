2f7a10b9734f2ac6932c5087620703bc68b58e15
Ema Vieira <er.vieira@campus.fct.unl.pt>
2020-11-11 19:15:40 +0000

    Changed try

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -76,12 +76,12 @@ echo "Executing java"
 printf "%.2d.. " 0
 for j in {1..1}; do
 
-  for k in {1..2};do
+  for k in {1..2}; do
     try=$(k)
     user=$(id -u):$(id -g)
 
     node=$(nextnode 0)
-    oarsh -n $node docker exec -d node-00 ./start${j}.sh 0 $user "$@"
+    oarsh -n $node docker exec -d node-00 ./start${j}.sh 0 $user ${try} "$@"
 
     sleep 1
 
@@ -92,7 +92,7 @@ for j in {1..1}; do
       if [ $((($i + 1) % 10)) -eq 0 ]; then
         echo ""
       fi
-      oarsh -n $node docker exec -d node-${ii} ./start${j}.sh $i $user $try contact=node-00:10000 "$@"
+      oarsh -n $node docker exec -d node-${ii} ./start${j}.sh $i $user ${try} contact=node-00:10000 "$@"
     done
     sleep 65
 
