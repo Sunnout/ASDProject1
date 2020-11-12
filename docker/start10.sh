9d97cfaa63fe68a3b417eda690cf7985ec35e38c
Ema Vieira <er.vieira@campus.fct.unl.pt>
2020-11-12 16:26:44 +0000

    Tests 2 6 e 10

--- docker/start10.sh
+++ docker/start10.sh
@@ -6,5 +6,5 @@ try=$3
 shift
 shift
 shift
-java -DlogFilename=logs/node$idx-t10-$try -cp asdProj.jar Main -conf config.properties dissemination_and_membership=2 payload_size=1000000 broadcast_interval=2000 "$@" &> /proc/1/fd/1
+java -DlogFilename=logs/node$idx-t10-$try -cp asdProj.jar Main -conf config.properties dissemination_and_membership=2 payload_size=100000 broadcast_interval=2000 "$@" &> /proc/1/fd/1
 chown $user logs/node$idx-t10-$try.log
--- docker/start6.sh
+++ docker/start6.sh
@@ -6,5 +6,5 @@ try=$3
 shift
 shift
 shift
-java -DlogFilename=logs/node$idx-t6-$try -cp asdProj.jar Main -conf config.properties dissemination_and_membership=1 payload_size=1000000 broadcast_interval=2000 clear_received_time=7000 "$@" &> /proc/1/fd/1
+java -DlogFilename=logs/node$idx-t6-$try -cp asdProj.jar Main -conf config.properties dissemination_and_membership=1 payload_size=100000 broadcast_interval=2000 clear_received_time=7000 "$@" &> /proc/1/fd/1
 chown $user logs/node$idx-t6-$try.log
